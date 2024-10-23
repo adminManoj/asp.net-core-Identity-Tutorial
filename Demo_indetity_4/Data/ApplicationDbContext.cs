22a5c23a8abe44fc4ac4d9866b20736552971571
adminManoj <deshwal.03@gmail.com>
2024-10-23 11:33:56 +0530

    initial commit

---
+++ Demo_indetity_4/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Demo_indetity_4.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo_indetity_4.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ Demo_indetity_4/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace Demo_indetity_4.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
